4c5d5860b32e17982abc1de11b43d45675530776
weicai_wang <898906871@qq.com>
2018-04-14 21:10:27 +0800

    inite project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/**
+ * 主程序入口
+ * by weicai
+ */
+import routeConfig from "./route.config.js";
+
+if(module.hot){
+  module.hot.accept()
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        HOME!Hello!
+    </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    route() {
+      return this.$route;
+    }
+  },
+  mounted() {
+  }
+};
+</script>
+
+<style>
+
+</style>
